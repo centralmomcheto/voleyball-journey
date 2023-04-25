fce90f1f40a910cddd42e16613d4c989a78e5b78
Martin Rachev <mrachev06@gmail.com>
2023-04-25 11:44:01 +0300

    posledni promeni

--- player.java
+++ player.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Vector;
 public class player {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		c1 = new name();
 		 int [] [] matrix = {
 	    		 {4,3,2},
 	    		 {4,3,2},
@@ -12,7 +13,7 @@ public class player {
 	    		 {5,6,1},
 	    		 {5,6,1},
 	     };
-
+		 ArrayList<programa> player = new ArrayList<>();
 	}
 
 }
--- programa.java
+++ programa.java
@@ -1,9 +1,28 @@
 
 public class programa {
-String name;
-String surname;
-String country;
-double visochina;
-String team;
-int jerseynumber;
+	String name;
+	String surname;
+	String country;
+	double visochina;
+	String team;
+	int jerseynumber;
+	public programa(String i) {
+		name = i;
+	}
+	public programa(String f) {
+		surname = f;
+	}
+	public programa(String d) {
+		country = d;
+	}
+	public programa(double v) {
+		visochina = v;
+	}
+	public programa(String t) {
+		team = t;
+	}
+	public programa(int n) {
+		jerseynumber = n;
+	}
+
 }
